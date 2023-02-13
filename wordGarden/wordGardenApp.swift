d2c1a3fd290e2b7bd99cea1de0f47a7941d7fa08
Jonathan Wheeler <78514430+wheelejp@users.noreply.github.com>
2023-02-13 11:48:16 -0500

    Initial Commit

---
+++ wordGarden/wordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordGardenApp.swift
+//  wordGarden
+//
+//  Created by Jonathan Wheeler Jr. on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct wordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
